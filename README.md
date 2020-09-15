6e63a77d00352b2642328ed58ab37f76d8ea9b89
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-09-14 21:27:56 -0300

    finished challenge

---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center">Welcome to nodejs-concepts 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> Fundamentals from building a rest API
+
+## Install
+
+```sh
+yarn install
+```
+
+## Run tests
+
+```sh
+yarn run test
+```
+
+## Author
+
+👤 **lucas mallmann**
+
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,78 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0) {
+    return response.status(400).json({ error: "Repository does not exist" });
+  }
+
+  const updatedRepository = {
+    id,
+    likes: repositories[index].likes,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[index] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const foundRepository = repositories.findIndex((repo) => repo.id === id);
+
+  if (foundRepository < 0) {
+    return response
+      .status(400)
+      .json({ error: "This repository does not exist" });
+  }
+
+  repositories.splice(foundRepository, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const foundRepositoryIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (foundRepositoryIndex < 0) {
+    return response
+      .status(400)
+      .json({ error: "This repository does not exist" });
+  }
+
+  repositories[foundRepositoryIndex].likes += 1;
+
+  return response.json(repositories[foundRepositoryIndex]);
 });
 
 module.exports = app;
